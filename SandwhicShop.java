bc158d8a103805160ceb1764a116cb2db5fff529
ndelgado09 <ndelgado09@yahoo.com>
2017-08-02 19:19:30 -0500

    Add files via upload

---
+++ SandwhicShop.java
@@ -0,0 +1,64 @@
+
+import static java.lang.System.out;
+
+import java.util.Scanner;
+
+public class SandwhicShop
+{
+    public static void main (String [] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+        int goalForVeggies= 50;
+        int goalForBurgers= 250;
+        int goalForSubs= 180;
+        int goalForSoup= 70;
+        out.println("Checking sales goals.");
+        out.println("The sales goal for veggie sandwhiches is 50");
+        out.println (" How many veggies sandwhich were sold today?");
+        int gVeggies = keyboard.nextInt();
+
+        if (gVeggies >=goalForVeggies){
+            out.println("Made goal for veggies.");
+        }else{
+            out.println ("Actaul value is less than goal");
+        }
+
+        out.println ("The sales goal for burgers is 250");
+        out.println ("How many burgers were sold today");
+        int gBurgers = keyboard.nextInt();
+
+        if (gBurgers>= goalForBurgers){
+            out.println("Made goal for Burgers.");
+        }else{
+            out.println ("Actual value is less than goal.");
+        }
+
+        out.println ("The sales goal for subs is 180");
+        out.println ("How many subs were sold today?");
+        int gSubs = keyboard.nextInt();
+
+        if (gSubs>=goalForSubs){
+            out.println ("Made goal for subs.");
+        }else{
+            out.println ("Actual value is less than goal.");
+        }
+
+        out.println("The sales goal for soup is 70");
+        out.println("How many soups were sold today?");
+        int gSoup = keyboard.nextInt();
+
+        if (gSoup>=goalForSoup){
+            out.println ("Made goal for soup.");
+        }else{
+            out.println ("Actual value is less than goal.");
+        }
+
+        if (gVeggies>=goalForVeggies && gBurgers>=goalForBurgers && gSubs>=goalForSubs &&
+        gSoup>=goalForSoup){
+            out.print("Made goal for everything!");
+        }
+    }
+
+}
+
+
